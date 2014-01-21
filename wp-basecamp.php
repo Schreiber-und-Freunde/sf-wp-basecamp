a9074be4912dca1f20b7fcd1ddd0e027ed06cd46
Philipp Schreiber <ps@schreiber-freunde.de>
2014-01-21 15:37:09 +0100

    Adds plugin header

---
+++ wp-basecamp.php
@@ -0,0 +1,9 @@
+<?
+/*
+Plugin Name: WP Basecamp
+Description: Basecamp API Integration for Wordpress
+Version: 0.1
+Author: Schreiber & Freunde GmbH
+Author URI: http://www.schreiber-freunde.de
+*/
+?>
